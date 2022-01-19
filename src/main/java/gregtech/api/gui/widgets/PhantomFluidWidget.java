886c2251040f9e98295e62dc09fb5599e4a76e71
PrototypeTrousers <salomaoo@gmail.com>
2022-01-19 15:25:32 -0300

    allow fluid filter to be cleared with left-clicking with an empty hand

--- src/main/java/gregtech/api/gui/widgets/PhantomFluidWidget.java
+++ src/main/java/gregtech/api/gui/widgets/PhantomFluidWidget.java
@@ -221,6 +221,9 @@ public class PhantomFluidWidget extends Widget implements IIngredientSlot, IGhos
                         }
                     }
                 }
+                else {
+                    fluidStackUpdater.accept(null);
+                }
             }
         } else if (id == 2) {
             FluidStack fluidStack;
