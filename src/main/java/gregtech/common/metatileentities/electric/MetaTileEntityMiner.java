6e5547cd015d541f414de97739f26aa5cb8ea6b6
ChromaPIE <chromasetha@outlook.com>
2022-02-21 15:44:30 +0800

    test

--- src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityMiner.java
+++ src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityMiner.java
@@ -130,6 +130,9 @@ public class MetaTileEntityMiner extends TieredMetaTileEntity implements IMiner,
     }
 
     private void addDisplayText(@Nonnull List<ITextComponent> textList) {
+        ITextComponent startText = new TextComponentString(I18n.format("gregtech.machine.miner.start"));
+        withHoverTextTranslate(startText, "gregtech.machine.miner.startinfo");
+        textList.add(startText);
         textList.add(new TextComponentString(I18n.format("gregtech.machine.miner.startx", this.minerLogic.getX().get())));
         textList.add(new TextComponentString(I18n.format("gregtech.machine.miner.starty", this.minerLogic.getY().get())));
         textList.add(new TextComponentString(I18n.format("gregtech.machine.miner.startz", this.minerLogic.getZ().get())));
