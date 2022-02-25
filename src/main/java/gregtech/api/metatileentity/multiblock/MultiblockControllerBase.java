e42ce8f2e3c13b2de3c21901257cb74a08094564
ChromaPIE <chromasetha@outlook.com>
2022-02-26 01:37:30 +0800

    Revert "would it fix？TrackedDummyWorld"
    This reverts commit 4c7333dcc74e4380913b2d8ccbf58ee6adb4edd0.

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiblockControllerBase.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiblockControllerBase.java
@@ -78,11 +78,9 @@ public abstract class MultiblockControllerBase extends MetaTileEntity implements
             if (getOffsetTimer() % 20 == 0 || isFirstTick()) {
                 checkStructurePattern();
             }
-        }
-        // TrackedDummyWorld is the world for the JEI preview. We do not want to update the Multi in this world,
-        // besides initially forming it in checkStructurePattern
-        if (isStructureFormed() && !(getWorld() instanceof TrackedDummyWorld)) {
-            if (!getWorld().isRemote) {
+            // TrackedDummyWorld is the world for the JEI preview. We do not want to update the Multi in this world,
+            // besides initially forming it in checkStructurePattern
+            if (isStructureFormed() && !(getWorld() instanceof TrackedDummyWorld)) {
                 updateFormedValid();
             }
         }
