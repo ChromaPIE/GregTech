69a8a52c150c5ecb95a498d5f241a88f49e9bb1d
ChromaPIE <chromasetha@outlook.com>
2022-03-04 11:12:12 +0800

    Revert "！hpfl fixing bbuffer facing"
    This reverts commit 930f5a470af874996de36ce7fe7f06b7cb03ad13.

--- src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityBatteryBuffer.java
+++ src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityBatteryBuffer.java
@@ -23,7 +23,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
@@ -42,8 +41,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MetaTileEntityBatteryBuffer extends TieredMetaTileEntity implements IControllable, IDataInfoProvider {
-
-    private BlockPos frontPos;
 
     private final int inventorySize;
     private boolean allowEnergyOutput = true;
@@ -77,11 +74,6 @@ public class MetaTileEntityBatteryBuffer extends TieredMetaTileEntity implements
             return GregtechTileCapabilities.CAPABILITY_CONTROLLABLE.cast(this);
         }
         return super.getCapability(capability, side);
-    }
-
-    @Override
-    public void onFrontFacingSet(EnumFacing newFrontFacing) {
-        this.frontPos = null;
     }
 
     @Override
